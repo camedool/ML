76086c5fad7f38c6de792bf78e295524109b69a8
camedool <2long4live@gmail.com>
2018-04-06 01:38:13 +0200

    cleaned the code

--- makeClassification.py
+++ makeClassification.py
@@ -3,18 +3,10 @@
 Created on Fri Apr  6 00:35:18 2018
 
 """
-import sys,os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-stderr = sys.stderr
-sys.stderr = open(os.devnull, 'w')
 from keras.models import load_model
 import numpy as np
 from keras.preprocessing.text import Tokenizer
 import pandas as pd
-import argparse as swr
-
-import loadingData
-
 
 classes = {'HRIS - Learning Link':0,'HRIS - CompetencePortal':1,
  'HRIS - Access Management':2,'HRIS - Legacy - AC Staff':3,
@@ -31,20 +23,12 @@ model = load_model('text_classifier.h5')
 
 def predict(str_query):
 
-    #tokenizer = Tokenizer(num_words=numwords)
     X_raw_test = [str_query]
-    #df = loadingData.loadCleanData()
-    #X_raw = df['short_description'].values
-    #tokenizer.fit_on_texts(X_raw)
     x_test = tokenizer.texts_to_matrix(X_raw_test, mode='binary')
     prediction = model.predict(np.array(x_test))
     class_num = np.argmax(prediction[0])
-    #class_prob = np.argmax(prediction[1])
-    sys.stderr = stderr
+    
     for name, index in classes.items():
         if index == class_num:
             print("%s: %f%% confidence" % (name, prediction[0][np.argmax(prediction)]*100))
     return prediction
-
-def pr(query):
-    return predict(query)
